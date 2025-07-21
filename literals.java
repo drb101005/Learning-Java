59a5a2104417e9b5f11d71ac612d43211495ad7a
drb101005 <drb101005@gmail.com>
2025-07-21 22:05:21 +0530

    Create literals.java

---
+++ literals.java
@@ -0,0 +1,16 @@
+public class literals {
+    public static void main(String[] args) {
+        int age = 24;
+        byte agee = 34;
+        short ageee = 44;
+        char che = 'd';
+        float nume = 21.89f;
+
+        System.out.println(age);
+        System.out.println(agee);
+        System.out.println(ageee);
+        System.out.println(che);
+        System.out.println(nume);
+    }
+    
+}
