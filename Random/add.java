557be9f0f0636a44e5e5746564279ebaa8bb9c45
drb101005 <drb101005@gmail.com>
2025-07-21 13:59:40 +0530

    Create add.java

---
+++ Random/add.java
@@ -0,0 +1,12 @@
+public class add {
+
+    public static void main(String[] args){
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+        System.out.print(sum);
+       
+
+    }
+}
