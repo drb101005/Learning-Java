5d8e5409b9eb842870d1c560ab2fb047cc6765a5
drb101005 <drb101005@gmail.com>
2025-08-07 08:46:28 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[]args){
+        byte age = 25;
+        short age2 = 123;
+        int age3 = 159_456_124;
+        long Total_views = 789_799_134_753l;
+        float cgps = 9.88f;
+        char name = 'D';
+        
+
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(Total_views);
+        System.out.println(cgps);
+        System.out.println(name);
+        
+
+
+    }
+}
